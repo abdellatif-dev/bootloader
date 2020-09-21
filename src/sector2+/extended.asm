46a754b9b3c0d92da9d0ec7e017cd5d16ecc4b9e
flagmate <flagmate@dhcppc4.linux>
2020-09-21 10:52:30 -0400

    32 bits mode

--- src/sector2+/extended.asm
+++ src/sector2+/extended.asm
@@ -1,12 +1,39 @@
 [org 0x7e00]
 
-mov bx, welcome
-call printf
-
-jmp $
+jmp Enterprotecedmode
 
 %include "./src/lib/printf.asm"
 
-welcome: db "tvOS", 0
+%include "./src/sector2+/gdt.asm"
+
+Enterprotecedmode:
+    call EnableA20
+    cli
+    lgdt [gdt_descriptor]
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+ 
+    jmp codeseg:Startprotecedmode
+
+EnableA20:
+    in al,0x92
+    or al, 2
+    out 0x92, al 
+
+    ret
+
+[bits 32]
+
+Startprotecedmode:
+    mov ax, dataseg
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    jmp $
+
 
 times 2048-($-$$) db 0
