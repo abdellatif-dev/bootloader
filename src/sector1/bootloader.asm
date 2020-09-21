ddd0416764367075ca99fd972b60c7227f9036db
flagmate <flagmate@dhcppc4.linux>
2020-09-21 09:38:43 -0400

    tested sectors

--- src/sector1/bootloader.asm
+++ src/sector1/bootloader.asm
@@ -5,19 +5,12 @@ mov [BOOT_DISK], dl
 mov bp, 0x7c00
 mov sp, bp
 
-mov bx, welcome
-call printf
 call Read_Disk
 
-mov ah, 0x0e
-mov al, [SOFTWARE_SPACE]
-int 0x10
 jmp $
 
 %include "./src/sector1/lib/printf.asm"
 %include "./src/sector1/lib/diskread.asm"
-
-welcome: db 'welcome to TvOS',0
 
 times 510-($-$$) db 0
 dw 0xaa55
